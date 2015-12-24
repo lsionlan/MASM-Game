dfbd89e2bdc5c83ff08c5e3018a00a79e9639848
lsionlan <lsi684106@gmail.com>
2015-12-24 16:32:23 +0800

    Update start(1).asm
    Only left ranks link.

--- src/start(1).asm
+++ src/start(1).asm
@@ -1,22 +1,41 @@
 TITLE start
 ;INCLUDE score.asm
 .data
-	space     		BYTE "             ",0
+	textStr			BYTE "Foreword:",0
+	textStr1		BYTE "Enter is equal to enter spacebar twice.",0
+	entgaStr		BYTE "Please enter the spacebar to continue.",0
+	space     		BYTE "              ",0
 	space2     		BYTE "                ",0
-	cStr		 	BYTE "Press spacebar to continue", 0	
+	cStr		 	BYTE "continue ( spacebar )", 0	
 	readynumber1 	BYTE "  3  ", 0
 	readynumber2 	BYTE "  2  ", 0
 	readynumber3 	BYTE "  1  ", 0
 	goStr			BYTE "GO!!!", 0
 	exStr1			BYTE "Instructions:",0
-	exStr2			BYTE "Please use the spacebar to jump",0
+	exStr2			BYTE "Please use the spacebar to jump.",0
 	opt1			BYTE "Start",0
 	opt2			BYTE "Rank",0
 	opt3			BYTE "Instructions",0
+	Bstr			BYTE "Back ( b )",0	
 .code
-start PROC
+start PROC	
+STARTSCREEN:
+	call	ClrScr	
+	mov		edx, OFFSET[textStr]
+	call 	WriteString
+	call 	Crlf
+	mov		edx, OFFSET[textStr1]
+	call 	WriteString	
+	call 	Crlf
+	mov		edx, OFFSET[entgaStr]
+	call 	WriteString
+	call 	ReadChar
+	cmp 	al, ' '
+	je		GAMECH
+TES:
+	jmp 	STARTSCREEN
+GAMECH:
 	call	ClrScr
-STARTACREEN:
 	call 	Crlf
 	call 	Crlf
 	call 	Crlf
@@ -28,12 +47,16 @@ STARTACREEN:
 	call 	Crlf
 	call 	Crlf
 	call 	Crlf
-	mov 	edx, OFFSET[space2] 
-	call 	WriteString
-	call 	WriteString
-	mov 	edx, OFFSET opt1
-	call 	WriteString
-	call 	Crlf
+	mov 	edx, OFFSET[space2] 		;G
+	call 	WriteString					;A
+	call 	WriteString					;M
+	mov		eax,white+(blue*16)			;E
+	call	SetTextColor				;S
+	mov 	edx, OFFSET opt1			;T
+	call 	WriteString					;A
+	mov		eax,white+(black*16)		;R
+	call	SetTextColor				;T
+	call 	Crlf						
 	mov 	edx, OFFSET[space2] 
 	call 	WriteString
 	call 	WriteString
@@ -44,7 +67,63 @@ STARTACREEN:
 	call 	WriteString
 	call 	WriteString
 	mov 	edx, OFFSET opt3
+	call 	WriteString	
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf 
+	call 	Crlf
+	call 	Crlf
+	mov 	edx, OFFSET[space] 
 	call 	WriteString
+	call 	WriteString
+	mov 	edx, OFFSET[cStr] ; Tell user press spacebar to start.
+	call 	WriteString	
+	call 	ReadChar
+	cmp 	al, 's'
+	je		RANK
+TES1:
+	call 	ReadChar  
+	cmp 	al, ' '
+	je		READY
+RANK:
+	call	ClrScr
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	mov 	edx, OFFSET[space2] 		;
+	call 	WriteString					;
+	call 	WriteString					;
+	mov 	edx, OFFSET opt1			;
+	call 	WriteString					;
+	call 	Crlf						;
+	mov 	edx, OFFSET[space2] 		;
+	call 	WriteString					;
+	call 	WriteString					;
+	mov		eax,white+(blue*16)			;
+	call	SetTextColor				;
+	mov 	edx, OFFSET opt2			;
+	call 	WriteString					;
+	call 	Crlf						;
+	mov		eax,white+(black*16)		;
+	call	SetTextColor
+	mov 	edx, OFFSET[space2] 
+	call 	WriteString
+	call 	WriteString
+	mov 	edx, OFFSET opt3
+	call 	WriteString	
 	call 	Crlf
 	call 	Crlf
 	call 	Crlf
@@ -60,13 +139,66 @@ STARTACREEN:
 	call 	WriteString
 	mov 	edx, OFFSET[cStr] ; Tell user press spacebar to start.
 	call 	WriteString
+TES2:
+	call 	ReadChar  
+	cmp 	al, 's'
+	jmp		INST
+INST:
+	call	ClrScr
 	call 	Crlf
-RETRY:
-	call 	ReadChar  ; If user press spacebar then continue.
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	mov 	edx, OFFSET[space2] 		;
+	call 	WriteString					;
+	call 	WriteString					;
+	mov 	edx, OFFSET opt1			;
+	call 	WriteString					;
+	call 	Crlf						;
+	mov 	edx, OFFSET[space2] 		;
+	call 	WriteString					;
+	call 	WriteString					;					
+	mov 	edx, OFFSET opt2			;
+	call 	WriteString					;
+	call 	Crlf						;	
+	mov 	edx, OFFSET[space2] 
+	call 	WriteString
+	call 	WriteString
+	mov		eax,white+(blue*16)			
+	call	SetTextColor
+	mov 	edx, OFFSET opt3
+	call 	WriteString
+	mov		eax,white+(black*16)		
+	call	SetTextColor	
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	call 	Crlf
+	mov 	edx, OFFSET[space] 
+	call 	WriteString
+	call 	WriteString
+	mov 	edx, OFFSET[cStr] ; Tell user press spacebar to start.
+	call 	WriteString
+	call 	ReadChar  
+	cmp 	al, 's'
+	je		GAMECH
+TES3:
+	call 	ReadChar 
 	cmp 	al, ' '
 	je		EX
-FAIL1:
-	jmp 	RETRY
 EX:
 	call	ClrScr
 	mov 	edx, OFFSET[exStr1]     ; rule ex
@@ -96,15 +228,12 @@ EX:
 	call 	Crlf
 	call 	Crlf
 	call 	Crlf
-	mov 	edx, OFFSET[space] 
-	call 	WriteString
-	call 	WriteString
-	mov 	edx, OFFSET[cStr] ; Tell user press spacebar to start.
+	mov 	edx, OFFSET[bStr] ; Tell user press spacebar to start.
 	call 	WriteString
 	call 	ReadChar  ; If user press spacebar then continue.
-	cmp 	al, ' '
-	je		READY
-FAIL2:
+	cmp 	al, 'b'
+	je		GAMECH
+FAIL2:	
 	jmp 	EX
 READY:
 	call    ClrScr
