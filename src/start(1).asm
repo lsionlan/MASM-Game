d7692c419587af43ba51be43680d71a1461fbcf0
lsionlan <lsi684106@gmail.com>
2015-12-27 18:06:58 +0800

    Update start(1).asm

--- src/start(1).asm
+++ src/start(1).asm
@@ -2,13 +2,14 @@ TITLE start
 .data
 	textStr			BYTE "Foreword:",0
 	textStr1		BYTE "Enter is equal to enter spacebar twice.",0
+	textStr2		BYTE "Please enter the 's' to choose the next option.",0
 	entgaStr		BYTE "Please enter the spacebar to continue.",0
 	space     		BYTE "              ",0
 	space2     		BYTE "                ",0
 	cStr		 	BYTE "continue ( spacebar )", 0	
-	readynumber1 	BYTE "  3  ", 0
-	readynumber2 	BYTE "  2  ", 0
-	readynumber3 	BYTE "  1  ", 0
+	readynumber1 		BYTE "  3  ", 0
+	readynumber2 		BYTE "  2  ", 0
+	readynumber3 		BYTE "  1  ", 0
 	goStr			BYTE "GO!!!", 0
 	exStr1			BYTE "Instructions:",0
 	exStr2			BYTE "Please use the spacebar to jump.",0
@@ -20,17 +21,20 @@ TITLE start
 start PROC	
 STARTSCREEN:
 	call	ClrScr	
-	mov		edx, OFFSET[textStr]
+	mov	edx, OFFSET[textStr]
 	call 	WriteString
 	call 	Crlf
-	mov		edx, OFFSET[textStr1]
+	mov	edx, OFFSET[textStr1]
 	call 	WriteString	
 	call 	Crlf
-	mov		edx, OFFSET[entgaStr]
+	mov	edx, OFFSET[textStr2]
+	call 	WriteString	
+	call 	Crlf
+	mov	edx, OFFSET[entgaStr]
 	call 	WriteString
 	call 	ReadChar
 	cmp 	al, ' '
-	je		GAMECH
+	je	GAMECH
 TES:
 	jmp 	STARTSCREEN
 GAMECH:
@@ -49,11 +53,11 @@ GAMECH:
 	mov 	edx, OFFSET[space2] 		;G
 	call 	WriteString					;A
 	call 	WriteString					;M
-	mov		eax,white+(blue*16)			;E
+	mov	eax,white+(blue*16)			;E
 	call	SetTextColor				;S
 	mov 	edx, OFFSET opt1			;T
 	call 	WriteString					;A
-	mov		eax,white+(black*16)		;R
+	mov	eax,white+(black*16)		;R
 	call	SetTextColor				;T
 	call 	Crlf						
 	mov 	edx, OFFSET[space2] 
@@ -84,11 +88,11 @@ GAMECH:
 	call 	WriteString	
 	call 	ReadChar
 	cmp 	al, 's'
-	je		RANK
+	je	RANK
 TES1:
 	call 	ReadChar  
 	cmp 	al, ' '
-	je		READY
+	je	READY
 RANK:
 	call	ClrScr
 	call 	Crlf
@@ -111,12 +115,12 @@ RANK:
 	mov 	edx, OFFSET[space2] 		;
 	call 	WriteString					;
 	call 	WriteString					;
-	mov		eax,white+(blue*16)			;
+	mov	eax,white+(blue*16)			;
 	call	SetTextColor				;
 	mov 	edx, OFFSET opt2			;
 	call 	WriteString					;
 	call 	Crlf						;
-	mov		eax,white+(black*16)		;
+	mov	eax,white+(black*16)		;
 	call	SetTextColor
 	mov 	edx, OFFSET[space2] 
 	call 	WriteString
@@ -141,7 +145,7 @@ RANK:
 TES2:
 	call 	ReadChar  
 	cmp 	al, 's'
-	jmp		INST
+	jmp	INST
 INST:
 	call	ClrScr
 	call 	Crlf
@@ -170,11 +174,11 @@ INST:
 	mov 	edx, OFFSET[space2] 
 	call 	WriteString
 	call 	WriteString
-	mov		eax,white+(blue*16)			
+	mov	eax,white+(blue*16)			
 	call	SetTextColor
 	mov 	edx, OFFSET opt3
 	call 	WriteString
-	mov		eax,white+(black*16)		
+	mov	eax,white+(black*16)		
 	call	SetTextColor	
 	call 	Crlf
 	call 	Crlf
@@ -193,11 +197,11 @@ INST:
 	call 	WriteString
 	call 	ReadChar  
 	cmp 	al, 's'
-	je		GAMECH
+	je	GAMECH
 TES3:
 	call 	ReadChar 
 	cmp 	al, ' '
-	je		EX
+	je	EX
 EX:
 	call	ClrScr
 	mov 	edx, OFFSET[exStr1]     ; rule ex
@@ -231,35 +235,35 @@ EX:
 	call 	WriteString
 	call 	ReadChar  ; If user press spacebar then continue.
 	cmp 	al, 'b'
-	je		GAMECH
+	je	GAMECH
 FAIL2:	
 	jmp 	EX
 READY:
 	call    ClrScr
-	mov		eax, 1000
+	mov	eax, 1000
 	call	Delay	
 
 	mov 	edx, OFFSET[readynumber1]	
 	call 	WriteString	
-	mov		eax, 1000
+	mov	eax, 1000
 	call	Delay
 	call    ClrScr
 
 	mov 	edx, OFFSET[readynumber2]
 	call 	WriteString
-	mov		eax, 1000
+	mov	eax, 1000
 	call	Delay
 	call    ClrScr
 
 	mov 	edx, OFFSET[readynumber3]
 	call 	WriteString
-	mov		eax, 1000
+	mov	eax, 1000
 	call	Delay
 	call    ClrScr
 
 	mov 	edx, OFFSET[goStr]
 	call 	WriteString
-	mov		eax, 1000
+	mov	eax, 1000
 	call	Delay
 	call    ClrScr
 start ENDP
